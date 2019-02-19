e65ec21485b87829038ef48744b0faa352ce867a
admin-zoujing <489548322@qq.com>
2019-02-19 08:24:54 +0800

    Update in.sh

--- in.sh
+++ in.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#使用docker attach有时会开住，并且退出会停止容器，使用下面脚本进入则可避免！
 #yum -y install util-linux
 CNAME=$1
 CPID=$(docker inspect --format "{{ .State.Pid }}" $CNAME)
 nsenter --target "$CPID" --mount --uts --ipc --net --pid
 
-#注意：安装util-linux包，使用nsenter.  用法：./in.sh mynginx
+#注意：安装util-linux包，使用nsenter.  用法：./dockerin.sh mynginx
