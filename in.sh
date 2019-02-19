7c304f59df57a7477ba0f2ccd42755609c1097c8
admin-zoujing <489548322@qq.com>
2019-02-19 08:26:14 +0800

    Update in.sh

--- in.sh
+++ in.sh
@@ -5,4 +5,4 @@ CNAME=$1
 CPID=$(docker inspect --format "{{ .State.Pid }}" $CNAME)
 nsenter --target "$CPID" --mount --uts --ipc --net --pid
 
-#注意：安装util-linux包，使用nsenter.  用法：./dockerin.sh mynginx
+#注意：安装util-linux包，使用nsenter.  用法：./in.sh mynginx
